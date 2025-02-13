dd028eadcbf7b5ad93a947bda4102e3798363289
VenkateshRK33 <venkateshrkulkarni1@gmail.com>
2025-02-14 00:29:56 +0530

    .
    .

---
+++ Area_of_circle.py
@@ -0,0 +1,4 @@
+from math import pi
+len=float(input("enter the radius off circle:"))
+area=pi*len**2
+print("area of circle with radius off"+str(len)+" is: "+str(area))
---
+++ tuple.py
@@ -0,0 +1,7 @@
+values=input("input some comma-sepated numbers")
+
+list=values.split(",")
+tuple=tuple(list)
+
+print('List:',list)
+print('tuple:',tuple)
